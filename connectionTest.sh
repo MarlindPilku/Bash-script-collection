9bf121e9a266b0fa6daac8c9037e483c2a9937a7
Marlind <marlindpilku@gmail.com>
2025-08-03 22:40:30 +0200

    Third Commit

---
+++ connectionTest.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# -c [arg] for number of counts
+
+
+
+read -p "Enter IP address: " address
+
+option=""
+arg=""
+
+while getopts :c: opt; do
+case "$opt" in
+c) option="$opt"
+arg="$OPTARG";;
+:) echo "Something"
+exit 1;;
+?) echo "Unknown option"
+exit 1;
+esac
+done
+
+if [ -z "$option" ] && [ -z "$arg" ];then
+echo
+else
+echo "Option is $option"
+echo "Argument is $arg"
+fi
+
+if [ -n "$address" ] && [ -n "$arg" ];then
+ping -c "$arg" "$address"
+ping -c "$arg" "$address" 1> ping.log
+exit 0
+elif [ -n "$address" ];then
+ping -c 30 "$address"
+ping -c 30 "$address" 1> ping.log
+exit 0
+else
+echo "Provide Address"
+exit 1
+fi
+
